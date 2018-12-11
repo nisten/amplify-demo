6cfe6f6821ee7ee5448fa73cc491d5bcbf0d53aa
nisten <nisten@outlook.com>
2018-12-11 08:18:43 -0500

    added axios

--- src/pagesDefinitions/Index/Index.jsx
+++ src/pagesDefinitions/Index/Index.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 import App from '../../containers/App';
 import TestComponent from '../../components/TestComponent/TestComponent';
+import Blog from '../../components/Blog/Blog';
 
 const IndexPage = () => (
   <App>
     <TestComponent />
+    <Blog />
   </App>
 );
 
