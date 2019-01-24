811091f71076f073642d939c341f0d8fc5378400
nisten <nisten@outlook.com>
2019-01-24 15:55:22 -0500

    demo

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -25,7 +25,7 @@ class Blog extends Component {
     render() {
         return (
     
-          <div className="text-center">
+          <div>
             {this.state.post.map(wp => {
                 return(
                 <div>
